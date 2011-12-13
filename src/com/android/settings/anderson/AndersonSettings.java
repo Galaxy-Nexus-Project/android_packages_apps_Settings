acff7d79a8ef0550653cac3c2578c08448fa721e
Chuck Haines <chaines@codemonkeyjava.com>
2011-12-13 11:04:38 -0500

    Notification light WIP

--- src/com/android/settings/anderson/AndersonSettings.java
+++ src/com/android/settings/anderson/AndersonSettings.java
@@ -1,31 +1,33 @@
 package com.android.settings.anderson;
 
-import android.content.IntentFilter;
-import android.content.SharedPreferences;
-import android.content.SharedPreferences.OnSharedPreferenceChangeListener;
-import android.media.AudioManager;
+import android.content.ContentResolver;
 import android.os.Bundle;
+import android.preference.Preference;
+import android.util.Log;
 
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
 
 public class AndersonSettings extends SettingsPreferenceFragment
-	implements OnSharedPreferenceChangeListener {
+	implements Preference.OnPreferenceChangeListener {
+	
+	private static final String TAG = "AndersonSettings";
 
 	@Override
     public void onCreate(Bundle icicle) {
         super.onCreate(icicle);
+        
+        //ContentResolver resolver = getContentResolver();
 
         addPreferencesFromResource(R.xml.anderson_settings);
-
+        
+        Log.e(TAG, "Initialized anderson settings window");
     }
 
-	
 	@Override
-	public void onSharedPreferenceChanged(SharedPreferences sharedPreferences,
-			String key) {
+	public boolean onPreferenceChange(Preference preference, Object newValue) {
 		// TODO Auto-generated method stub
-		
+		return false;
 	}
 
 }
